7660dbf3935c6bd8a3f3ca4c085a5a4101f73eef
wenlieZhu <zhuwenlie@126.com>
2020-03-31 08:47:43 +0800

    create menu

---
+++ src/chumanle/one/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package chumanle.one.hello;
+/**
+ * 这是第一个Java程序
+ * @author gwzwl
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
